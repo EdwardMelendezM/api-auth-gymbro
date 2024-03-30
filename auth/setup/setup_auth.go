41240b8eb913e43dc4942d77df90b5c8445e24bd
EdwardMelendezM <edward.melendez.mendigure@gmail.com>
2024-03-30 16:34:48 -0500

    add changes with setup

--- auth/setup/setup_auth.go
+++ auth/setup/setup_auth.go
@@ -1,5 +1,22 @@
 package setup
 
-func LoadAuth() {
-	// LoadAuth
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/EdwardMelendezM/info-code-api-shared-v1/clock"
+
+	authDomain "github.com/EdwardMelendezM/api-auth/auth/infrastructure/persistence/mysql"
+	authHttpDelivery "github.com/EdwardMelendezM/api-auth/auth/interfaces"
+	authUseCase "github.com/EdwardMelendezM/api-auth/auth/usecase"
+)
+
+func LoadAuth(router *gin.Engine) {
+	timeoutContext := time.Duration(60) * time.Second
+	newClock := clock.NewClock()
+
+	authRepository := authDomain.NewAuthRepository(newClock, 60)
+	authUCase := authUseCase.NewAuthUseCase(authRepository, timeoutContext)
+	authHttpDelivery.NewAuthHandler(authUCase, router)
 }
